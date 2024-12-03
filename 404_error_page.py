7609792f7964c95439f2e45e3abacaa60f3bc3dd
Amelia Soucy <asoucy1@babson.edu>
2024-12-03 14:15:40 -0500

    Read me updates

--- 404_error_page.py
+++ 404_error_page.py
@@ -1,19 +1,19 @@
-#from fastapi import FastAPI, Request
-#from fastapi.responses import HTMLResponse
-#from fastapi.exceptions import HTTPException
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.exceptions import HTTPException
 
-#app = FastAPI()
+app = FastAPI()
 
-#@app.get("/")
-#def read_main():
-    #"""
-    #Uses FastAPI to provide a welcome message when customers access the main HTML page.
-    #"""
-    #return {"message": "Welcome to AD's Michael Kors PopUp!"}
+@app.get("/")
+def read_main():
+    """
+    Uses FastAPI to provide a welcome message when customers access the main HTML page.
+    """
+    return {"message": "Welcome to AD's Michael Kors PopUp!"}
 
-#@app.exception_handler(404)
-#async def custom_404(request: Request, e: HTTPException):
-    #"""
-    #Uses FastAPI to create a custom page for 404 errors.
-    #"""
-    #return HTMLResponse(content="<html><body><h1>Page Not Found</h1></body></html>", status_code=404)
+@app.exception_handler(404)
+async def custom_404(request: Request, e: HTTPException):
+    """
+    Uses FastAPI to create a custom page for 404 errors.
+    """
+    return HTMLResponse(content="<html><body><h1>Page Not Found</h1></body></html>", status_code=404)
